19d4a3c96d4c9a190303b6d3ee1ce5f70b362ba2
robas <pax.profundis@gmail.com>
2018-05-23 00:05:32 -0300

    data scrapper for ingredients, portions and calories

---
+++ ingredients-calories/ingredients_scraping.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import requests
+from bs4 import BeautifulSoup
+
+# Retrieving food categories links from www.tabeladecalorias.net
+def get_links(url):
+    links = list()
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    menu = soup.find('ul', attrs={'id':'menu-calorie-tables'})
+    for menu_item in menu.find_all('li'):
+        links.append(str(menu_item.find('a',href=True)['href']))
+    return links
+
+def get_ingredients(url):
+    ingredients = list()
+    page = requests.get(url)
+    # page = requests.get(url.encode('utf8'))
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # html = list(soup.children)[1]
+    # body = list(html.children)[1]
+    # processar cada string para criar os objetos 'ingredient'
+    ingredients_table = soup.find('table', attrs={'id':'calories-table'})
+    for ingredient in ingredients_table.find_all('tr', attrs={'class':'kt-row'}):
+        ingredient_name = ingredient.find('td', attrs={'class':'food'}).get_text().encode('utf-8')
+        ingredient_portion = ingredient.find('td', attrs={'class':'serving portion'}).get_text().encode('utf-8')
+        ingredient_calories = ingredient.find('td', attrs={'class':'kcal'}).get_text().encode('utf-8')
+        ingredients.append(ingredient_name + "," + ingredient_portion + "," + ingredient_calories)
+    return ingredients
+
+
+url = 'http://www.tabeladecalorias.net/'
+links = get_links(url)
+for link in links:
+    print(link)
+    ingredients = get_ingredients(link)
+    with open('ingredients.csv','ab') as file:
+        for ingredient in ingredients:
+            file.write(ingredient)
+            file.write('\n')
